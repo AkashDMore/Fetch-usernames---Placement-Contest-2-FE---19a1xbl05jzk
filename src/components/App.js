5a8e979a292f57f0c803de3f0db5ee9d1a7208a1
AkashMore <akashmoref121438@gmail.com>
2022-10-09 10:43:38 +0000

    Update package-lock.json and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import '../styles/App.css';
-import { useState, useEffect } from 'react';
+import React from "react";
+import "../styles/App.css";
+import { useState, useEffect } from "react";
 const App = () => {
-//code here 
- 
-
-
+  //code here
+  const [name, setName] = useState([]);
+  const [id, setId] = useState(1);
+  useEffect(() => {
+    fetch(`https://content.newtonschool.co/v1/pr/main/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data.name);
+        setName(data.name);
+      });
+  }, [id]);
+  const changeInput = (e) => {
+    setId(e.target.value);
+  };
 
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
-      <input id="input" onChange={changeInput} />
+      <input id="input" onChange={(e) => changeInput(e)} />
       <p id="name">{name}</p>
     </div>
   );
-}
-
+};
 
 export default App;
